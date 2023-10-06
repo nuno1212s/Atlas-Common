1749b9bb164372e06c39f1a80f7c3f2ce5c37a18
Nuno Neto <nuno.neto.g@gmail.com>
2023-10-07 00:37:16 +0100

    Work more on the decision log. Fix some things with state messages are handled to support the new phased approach. Still have to adapt the ordering protocol and the log transfer protocol to support the new features.

--- src/maybe_vec/mod.rs
+++ src/maybe_vec/mod.rs
@@ -9,12 +9,12 @@ use std::iter;
 /// speed and efficiency of stack allocation when we have a single value
 #[derive(Clone, Debug)]
 pub enum MaybeVec<T> {
+    None,
     One(T),
-    Vec(Vec<T>)
+    Vec(Vec<T>),
 }
 
 impl<T> MaybeVec<T> {
-
     pub fn from_one(member: T) -> Self {
         Self::One(member)
     }
@@ -23,7 +23,39 @@ impl<T> MaybeVec<T> {
         Self::Vec(objects)
     }
 
-    pub fn iter(&self) -> impl Iterator<Item = &T> {
+    pub fn builder() -> MaybeVecBuilder<T> {
+        MaybeVecBuilder::empty()
+    }
+
+    pub fn len(&self) -> usize {
+        match self {
+            MaybeVec::One(_) => {
+                1
+            }
+            MaybeVec::Vec(vec) => {
+                vec.len()
+            }
+            MaybeVec::None => {
+                0
+            }
+        }
+    }
+
+    pub fn is_empty(&self) -> bool {
+        match self {
+            MaybeVec::One(_) => {
+                false
+            }
+            MaybeVec::Vec(vec) => {
+                vec.is_empty()
+            }
+            MaybeVec::None => {
+                true
+            }
+        }
+    }
+
+    pub fn iter(&self) -> impl Iterator<Item=&T> {
         match self {
             MaybeVec::One(one) => {
                 iter::once(one)
@@ -31,10 +63,13 @@ impl<T> MaybeVec<T> {
             MaybeVec::Vec(vec) => {
                 vec.iter()
             }
+            MaybeVec::None => {
+                iter::empty()
+            }
         }
     }
 
-    pub fn iter_mut(&mut self) -> impl Iterator<Item = &mut T> {
+    pub fn iter_mut(&mut self) -> impl Iterator<Item=&mut T> {
         match self {
             MaybeVec::One(one) => {
                 iter::once(one)
@@ -42,10 +77,13 @@ impl<T> MaybeVec<T> {
             MaybeVec::Vec(vec) => {
                 vec.iter_mut()
             }
+            MaybeVec::None => {
+                iter::empty()
+            }
         }
     }
 
-    pub fn into_iter(self) -> impl Iterator<Item = T> {
+    pub fn into_iter(self) -> impl Iterator<Item=T> {
         match self {
             MaybeVec::One(obj) => {
                 iter::once(obj)
@@ -53,7 +91,62 @@ impl<T> MaybeVec<T> {
             MaybeVec::Vec(vec) => {
                 vec.into_iter()
             }
+            MaybeVec::None => {
+                iter::empty()
+            }
         }
+    }
+
+    pub fn into_vec(self) -> Vec<T> {
+        match self {
+            MaybeVec::None => {
+                Vec::new()
+            }
+            MaybeVec::One(val) => {
+                vec![val]
+            }
+            MaybeVec::Vec(vec) => {
+                vec
+            }
+        }
+    }
+}
+
+#[derive(Clone, Debug)]
+pub struct MaybeVecBuilder<T> {
+    current_value: MaybeVec<T>,
+}
+
+impl<T> MaybeVecBuilder<T> {
+
+    pub fn empty() -> Self {
+        Self {
+            current_value: MaybeVec::None
+        }
+    }
+
+    pub fn push(&mut self, value: T) {
+
+        let current = std::mem::replace(&mut self.current_value, MaybeVec::None);
+
+        self.current_value = match current {
+            MaybeVec::None => {
+                MaybeVec::One(value)
+            }
+            MaybeVec::One(curr_value) => {
+                MaybeVec::Vec(vec![curr_value, value])
+            }
+            MaybeVec::Vec(mut vec) => {
+                vec.push(value);
+
+                MaybeVec::Vec(vec)
+            }
+        };
+
+    }
+
+    pub fn build(self) -> MaybeVec<T> {
+        self.current_value
     }
 
 }
