d2eea01c454b69758af0dc6f5aa83fdd67a2c822
Nuno Neto <nuno.neto.g@gmail.com>
2024-01-14 14:37:46 +0000

    Done a lot more work on the separation of the types. Made Request and Reply session based types defined in core, since they that is only used by the protocol to help the clients know which request a given reply (and vice-versa) is meant to.

--- src/serialization_helper/mod.rs
+++ src/serialization_helper/mod.rs
@@ -5,15 +5,15 @@ use serde::{Deserialize, Serialize};
 /// have multiple declarations (which made parsing the project a whole lot harder for the compiler
 /// leading to lots of stupid "errors" which were actually just the compiler being "dumb")
 #[cfg(feature = "serialize_serde")]
-pub trait SerType: for<'a> Deserialize<'a> + Serialize + Send + Clone {}
+pub trait SerType: 'static + for<'a> Deserialize<'a> + Serialize + Send + Clone {}
 
 #[cfg(feature = "serialize_capnp")]
-pub trait SerType: Send + Clone {}
+pub trait SerType: 'static + Send + Clone {}
 
 /// Automatically implement the SerType trait for all types that implement the serde traits
 /// which, since we do not require any function impls, is the only thing we require
 #[cfg(feature = "serialize_serde")]
-impl<T> SerType for T where T: for<'a> Deserialize<'a> + Serialize + Send + Clone {}
+impl<T> SerType for T where T: 'static + for<'a> Deserialize<'a> + Serialize + Send + Clone {}
 
 #[cfg(feature = "serialize_capnp")]
-impl<T> SerType for T where T: Send + Clone {}
+impl<T> SerType for T where T: 'static + Send + Clone {}
