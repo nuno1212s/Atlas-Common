cc5401eee7a1bdfb58e13cac98213db14f6d6bb7
Nuno Neto <nuno.neto.g@gmail.com>
2024-01-15 00:24:06 +0000

    Fixed most compilation errors, still working on smr replica changes.

--- src/serialization_helper/mod.rs
+++ src/serialization_helper/mod.rs
@@ -4,6 +4,11 @@ use serde::{Deserialize, Serialize};
 /// An abstraction for the serializable types in this framework, so that they don't have to
 /// have multiple declarations (which made parsing the project a whole lot harder for the compiler
 /// leading to lots of stupid "errors" which were actually just the compiler being "dumb")
+///
+/// All SerTypes have to be 'static since they are used in multiple threads, which can live for the
+/// entire duration of the program, meaning we can't have non static references.
+///
+/// Any raw struct type with no internal references will follow this requirement
 #[cfg(feature = "serialize_serde")]
 pub trait SerType: 'static + for<'a> Deserialize<'a> + Serialize + Send + Clone {}
 
